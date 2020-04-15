a4c90dbb404e42bbb8b1d838f22e4c61230d956b
Khalil Acheche <achechekhalil@hotmail.fr>
2020-04-15 14:09:29 +0200

    w

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-var video = document.getElementsByTagName('video')[0];
-isAd=false;
-video.addEventListener('ended',function(e){
-  //console.log(video.getAttribute('src'));
-  if(!isAd)
-    setTimeout(nextEpisode,5000);
-},false);
-console.log(document.getElementsByClassName("slick-current")[0].nextSibling.firstChild.attributes);
-//nextEpisodeURL=document.getElementsByClassName("slick-current")[0].nextSibling.firstChild.getAttribute('episode-url');
-//console.log(nextEpisodeURL);
-video.onplay = function() {
-    isAd=video.getAttribute('src').search('blob')==-1;
+vid = document.getElementsByTagName('video')[0];
+var buton=document.getElementsByClassName('vjs-big-play-button')[0];
+vid.muted=true;
+autoplay();
+ vid.muted=false;
+var isAd=true;
+vid.onplay = function(){
+  isad=vid.getAttribute('src').search('blob')==-1;
+  console.log(isad);
 };
-function nextEpisode () {
-  console.log("next");
-    console.log(video.getAttribute('src').search('blob')>-1);// What you want to do after the event
+
+vid.onended=function(e){
+     console.log('end');
+      if(!isad)
+      {
+        setTimeout(nextEpisode,5000);
+      }
+    };
+
+function nextEpisode()
+{
+  var next=document.getElementsByClassName('slick-current')[0].nextSibling.firstElementChild;
+  window.location.replace(next.getAttribute('episode-url'));
+
+}
+function autoplay(){
+  var promise = document.querySelector('video').play();
+   if (promise !== undefined) {
+        promise.then(_ => {
+          console.log("Autoplay");
+          //$("button[title|='Fullscreen']").click();
+    }).catch(error => {
+       console.log(error);
+     });
+   }
 }
